deab823e14f7cb101d8290c7bf59f1739e05ccf7
Jamal Rahman <jamalrahman@github.com>
2020-03-19 20:32:29 +0000

    Started refactoring into OOP

--- src/scrape_data.py
+++ src/scrape_data.py
@@ -4,57 +4,90 @@ import time
 from collections import deque
 from pathlib import Path
 import os
-
-def cache_batch(batch, path):
-
-    # Make directories for saving batches
-    abs_path = (os.path.abspath(path))
-    Path(abs_path).mkdir(parents=True,exist_ok=True)
-    save_path = '{}/batch_{}.txt'.format(abs_path,batch_number)
-
-    print("Saving batch "+str(batch_number)+'to location '+str(save_path))
-    with open(save_path, 'w') as f:
-        for item in batch:
-            f.write(str(item))
+import requests
 
 
-f = codecs.open('data/applist.json','r','utf-8')
-data = json.load(f, encoding='utf-8')
-f.close()
+class GameScraper:
+    def __init__(batch_size, ms_per_batch):
+        self.batch_size = batch_size
+        self.ms_per_batch = ms_per_batch
+        self.current_batch_size = 0
+        self.batches_made = 0
+        self.batch = deque()
 
-print(len(data['applist']['apps']))
+    def scrape(data):
+        for app in data:
 
-batch_size = 200
-ms_per_batch = 1000
+            if(batches_made>0):
+                break;
 
-batch_starttime = datetime.now()
-current_batch_count = 0
-batch_number = 0
-batch = deque()
+            appid = str(app['appid'])
+            
+            # Make store.steampowered api call
+            response = requests.post('https://store.steampowered.com/api/appdetails/?appids='+appid)
 
-formatted_datetime = batch_starttime.strftime("%Y-%m-%d_%H-%M-%S")
-cache_path = 'data/cache/{}/'.format(formatted_datetime)
+            game_info = response.json()[appid]
+            if game_info['success'] == True:
+                is_game = game_info['data']['type']=='game'
+                print(app['name']+' '+str(is_game))
+
+                if is_game:
+                    app['is_game'] = int(is_game)
+                    batch.append(app)
+                    current_batch_size = current_batch_size+1
+
+            is_game = scraper.is_game(response)
+
+            if(self.current_batch_size==self.batch_size-1):
+                # cache batch
+                scraper.save_batch(batch, cache_path)
+
+                # wait for time_delta to hit ms_per_batch
+                batch_endtime = datetime.now()
+
+                timedelta = batch_endtime-batch_starttime;
+                time_to_wait = ms_per_batch - (timedelta.microseconds / 1000)
+                print('time to wait: '+str(time_to_wait))
+
+                time.sleep(max(time_to_wait,0)/1000)
+
+    def save_batch(batch, path):
+
+        # Make directories for saving batches
+        abs_path = (os.path.abspath(path))
+        Path(abs_path).mkdir(parents=True,exist_ok=True)
+        
+        # New file save path
+        save_path = '{}/batch_{}.txt'.format(abs_path,batch_number)
+
+        with open(save_path, 'w') as f:
+            for item in batch:
+                f.write(str(item))
+        
+        self.current_batch_size = 0
+        self.batch_number = batch_number+1
+
+    def is_game(appid,batch):
+        pass
+
+
+
+
+
+if __name__ == '__main__':
+
+    f = codecs.open('data/applist.json','r','utf-8')
+    data = json.load(f, encoding='utf-8')
+    f.close()
+
+    batch_starttime = datetime.now()
+    formatted_datetime = batch_starttime.strftime("%Y-%m-%d_%H-%M-%S")
+
+    cache_path = 'data/cache/{}/'.format(formatted_datetime)
     
+    scraper = GameScraper()
+    scraper.scrape(data['applist']['apps'])
+        
 
-for app in data['applist']['apps']:
-    if(current_batch_count==batch_size-1):
-        # cache batch
-        cache_batch(batch, cache_path)
-        current_batch_count = 0
-        batch_number = batch_number+1
-
-        # wait for time_delta to hit ms_per_batch
-        batch_endtime = datetime.now()
-        timedelta = batch_endtime-batch_starttime;
-        time_to_wait = ms_per_batch - (timedelta.microseconds / 1000)
-        print('time to wait: '+str(time_to_wait))
-
-        time.sleep(max(time_to_wait,0)/1000)
-
-    if(batch_number>1):
-        break;
-
-    appid = app['appid']
-    batch.append(appid)
-    current_batch_count = current_batch_count+1
-
+            # Assume we tried going too fast, so pause for 5 minutes and retry last.
+        
