dca0dc25ddd9a3ecfa78d9f7904a14665be67ef4
Jamal Rahman <jamalrahman@github.com>
2020-05-03 02:40:33 +0100

    docstrings

--- src/scrape_data.py
+++ src/scrape_data.py
@@ -6,63 +6,54 @@ from pathlib import Path
 import os
 import requests
 
-
-def attempt_func(func,element,iter=0,timeout=300):
-    try:
-        return func(element)
-    except TypeError:
-        if iter==0:
-            print('Excessive nulls, raising')
-            raise
-        else:
-            print('Null value, waiting')
-            time.sleep(timeout/1000)
-    attempt_func(func,element,iter+1)
-
-
 class CachingBatchProcessor:
-    def __init__(self,target_batch_size = 200, ms_per_req_batch = 300):
-        self.target_cache_size = 1000
+    """ This class executes any function similarly to the inbuilt python map() function, executing element-wise across an input list. In addition, the target function is rate-limited, executed in custom batches with periodic breaks between batches. Lastly, the outputs of the function are cached and regularly saved to disk, rather than being held in memory until completion of the map.
 
+    """    
+    def __init__(self,target_batch_size = 200, seconds_per_batch = 300, target_cache_size = 1000):
+        self.target_cache_size = target_cache_size
         self.target_batch_size = target_batch_size
-        self.ms_per_req_batch = ms_per_req_batch
-        self.current_requests = 0
+        self.seconds_per_batch = seconds_per_batch
 
+        self.current_requests = 0
         self.cache_number = 0
         self.current_cache_size = 0
-        self.caches_made = 0
         self.cache = deque()
     
 
 
-    def cache_process(self,func,data,save_path):
+    def map(self,func,data,save_path):
+        """ Maps a function across a dataset, caches & saves outputs regularly, while rate limiting function calls
+
+        Arguments:
+            func {function(a)} -- The function to batch
+            data {list(a)} -- An list of inputs to be processed by func one at a time
+            save_path {string} -- The save location for cached outputs
+        """
         batch_starttime = datetime.now()
         for element in data:
 
             # If we've hit the full batch, wait to start a new batch
             if(self.current_requests==self.target_batch_size):
-                # wait for time_delta to hit ms_per_batch
-
                 batch_endtime = datetime.now()
                 timedelta = batch_endtime-batch_starttime;
-                time_to_wait = self.ms_per_req_batch - (timedelta.total_seconds())
+                time_to_wait = self.seconds_per_batch - (timedelta.total_seconds())
                 print('time to wait: '+str(time_to_wait))
                 time.sleep(max(time_to_wait,0))
+
+                # Reset tracking metrics for a new batch
                 self.current_requests=0
                 batch_starttime = datetime.now()
 
             try:
                 self.current_requests = self.current_requests+1
                 print('current reqs: '+str(self.current_requests))
+                
+                # Attempt the function proper 
                 out = func(element)
-                # self.error_flag = False
             except:
-                # func(element) returned null more than n times, assume that element truly returns null, skip it
-                # if self.error_flag:
-                    # raise
-                # else:
+                # func(element) throws an error, attempt func(element) on the next element
                 print('Continuing to next element')
-                    # self.error_flag = True
                 continue
                 
             if out is not None:
@@ -72,16 +63,18 @@ class CachingBatchProcessor:
                 print('Number in cache '+str(self.current_cache_size))
             
             if(self.current_cache_size==self.target_cache_size):
-                # save cache
-                
-                self.save_cache(save_path)
+                self._save_cache(save_path)
 
-
-
+        # After all elements have been processed, ensure the final cache is saved
         if self.current_cache_size < target_cache_size:
-            self.save_cache(save_path)
+            self._save_cache(save_path)
 
-    def save_cache(self,save_path):
+    def _save_cache(self,save_path):
+        """Saves the contents of the executed func cache to disk
+
+        Arguments:
+            save_path {string} -- The path to a directory in which to save output
+        """
         abs_path = (os.path.abspath(save_path))
         Path(abs_path).mkdir(parents=True,exist_ok=True)
         
@@ -96,14 +89,38 @@ class CachingBatchProcessor:
         self.cache_number = self.cache_number+1
         self.cache = deque()
 
+    # def _attempt_func_internal(self,func, element, i, timeout, current_iter):
+    #     try:
+    #         return func(element)
+    #     except TypeError:
+    #         if current_iter==i:
+    #             print('Excessive nulls, raising')
+    #             raise
+    #         else:
+    #             print('Null value, waiting')
+    #             time.sleep(timeout/1000)
+    #     _attempt_func_internal(func,element,i,timeout,current_iter+1)
+
+    # def _attempt_func(self,func,element,i=0,timeout=300):
+    #     _attempt_func_internal(func,element,i,timeout,0)
+
 
         
-class SteamScraper:
-
-    def __init__(self):
-        self.features = ['is_free','developers','supported_languages','platforms','genres','categories','recommendations','price_overview','release_date']
+class SteamGameScraper:
+    """This class access the steampowered API to request details about a particular steam game
+    """
+    def __init__(self, scrape_properties):
+        self.scrape_properties = scrape_properties
 
     def scrape(self,data):
+        """Acquires a game's details from the store.steampowered API. Discards non-game titles.
+
+        Arguments:
+            data {dict} -- Dict of appid & game title, previously acquired from a different API
+
+        Returns:
+            dict -- Key-value pair of a game's properties
+        """
         appid = str(data['appid'])
 
         # Make store.steampowered api call
@@ -113,22 +130,27 @@ class SteamScraper:
         if game_data['success'] == True:
             is_game = game_data['data']['type']=='game'
             if is_game:
-                return self._extract_features(self.features,data,game_data)
+                return self._append_properties(data,game_data)
 
         return None
 
-    def _extract_features(self,features,data,game_data):
-        extracted = {}
-        for feature in features:
-            data[feature] = self._get_value(game_data['data'],feature)
-        return data
+    def _append_properties(self,data,game_data):
+        """Appends additional game-details to the existing appid/title dict
 
-    def _get_value(self,json,key):
-        try:
-            return json[key]
-        except:
-            return None
+        Arguments:
+            data {dict} -- Dict of appid & game title, previously acquired from a different API
+            game_data {json} -- Steam API response for the appid of the input data
+
+        Returns:
+            dict -- Dict of an app's appid, game title, and all chosen properties to extract
+        """
+        for prop in self.scrape_properties:
+            try:
+                data[prop] = game_data['data'][prop]
+            except:
+                data[prop] = None
             
+        return data
 
 
 
@@ -142,6 +164,9 @@ if __name__ == '__main__':
     formatted_datetime = datetime.strftime("%Y-%m-%d_%H-%M-%S")
 
     cache_path = '../data/cache/{}/'.format(formatted_datetime)
-    scraper = SteamScraper()
+
+    scrape_features = ['is_free','developers','supported_languages','platforms','genres','categories','recommendations','price_overview','release_date']
+    
+    scraper = SteamGameScraper(scrape_features)
     caching_processor = CachingBatchProcessor()
-    caching_processor.cache_process(scraper.scrape,data['applist']['apps'],cache_path)
+    caching_processor.map(scraper.scrape,data['applist']['apps'],cache_path)
