77126cdcb66afb1d16b750f8d17dda34102c50db
Jamal Rahman <jamalrahman@github.com>
2020-05-13 21:30:48 +0100

    API fully operational online

--- src/app.py
+++ src/app.py
@@ -1,18 +1,28 @@
 from flask import Flask, request
 from flask.views import MethodView
 
-from ml.models import NN, Model, Dummy
+from ml.models import NN, Model, Dummy, EmbeddingsRecommender
+
+import pandas as pd
+import ast
 
 app = Flask(__name__)
 
+training_data_path = '../data/prod/app_data.csv'
+app_data = pd.read_csv(training_data_path)
 
+path_to_model = '../models/doc2vec.pickle'
+model = EmbeddingsRecommender(path_to_model,app_data)
 # Detect if no model exists, set flag
 
 @app.route('/api',methods=['GET','POST'])
 def predict():
     if 'input' in request.args:
-        model_input = int(request.args['input'])
-        return {"output": str(2*model_input)}
+        model_input = request.args['input']
+        model_input = ast.literal_eval(model_input)
+        print(model_input)
+        out = model.predict(model_input)
+        return {"output": str(out)}
     else:
         return 'Error: No input given'
 
--- src/ml/models.py
+++ src/ml/models.py
@@ -2,8 +2,11 @@
 # Augment data with preprocessing
 # Data test/train split
 # Build CNN
-
-
+import gensim.models as g
+import pandas as pd
+from sklearn.preprocessing import MinMaxScaler
+import math
+scaler = MinMaxScaler()
 
 class Model():
     def __init__ (self,path:str=None):
@@ -54,3 +57,38 @@ class LogReg(Model):
         
     def predict(self, x):
         return self._model.predict(x)
+
+
+class EmbeddingsRecommender(Model):
+
+    def __init__(self, path=None,app_data=None):
+        super().__init__(path=path)
+        if self._model is None:
+            print('error handle me')
+        self._app_data = app_data
+
+    def predict(self, x):
+
+        # Cast x as list
+
+        positives = []
+        negatives = []
+    
+        for pos in x:
+            positives.append(self._model.docvecs[str(pos)])
+        
+        similarity = self._model.docvecs.most_similar(positive=positives, negative=negatives,topn=34071)
+        
+        similar_games = pd.DataFrame(similarity,columns=['appid','d2v_similarity'])
+        similar_games['appid'] = similar_games['appid'].astype(int)
+
+        similar_games = similar_games.merge(self._app_data[['appid','name','positive']], how='left',on='appid')
+        similar_games['scaled_positives'] = similar_games['positive']+1
+        similar_games['scaled_positives'] = similar_games['scaled_positives'].apply(math.log)
+        similar_games['scaled_positives'] = scaler.fit_transform(similar_games['scaled_positives'].values.reshape(-1,1))
+        similar_games['score'] = similar_games['d2v_similarity']+ 0.28*similar_games['scaled_positives']
+        similar_games = similar_games.sort_values(by='score',ascending=False)
+        return list(similar_games['name'])[:10]
+
+    def load(self):
+        self._model = g.Doc2Vec.load(self._path)
